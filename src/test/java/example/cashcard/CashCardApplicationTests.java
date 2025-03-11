f67593fa49dcdfba7a9dd0b2e120bb21eb831d97
Shivam Singh <shivam.singh0706@gmail.com>
2025-03-11 08:02:31 +0530

    authenticated, authorized Principal is used to create and get CashCards. CARD-OWNER can only get card which they own. CARD-OWNER can only create cashcard for themselves. Unit tests created.

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -49,7 +49,7 @@ class CashCardApplicationTests {
     @Test
     @DirtiesContext
     void shouldCreateANewCashCard() {
-        CashCard newCashCard = new CashCard(null, 250.00, "sarah1");
+        CashCard newCashCard = new CashCard(null, 250.00, null);
         ResponseEntity<Void> createResponse = restTemplate
                 .withBasicAuth("sarah1", "abc123")
                 .postForEntity("/cashcards", newCashCard, Void.class);
@@ -141,5 +141,13 @@ class CashCardApplicationTests {
                 .withBasicAuth("hank-owns-no-cards", "qrs456")
                 .getForEntity("/cashcards/99", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    void shouldNotAllowAccessToCashCardsTheyDoNotOwn() {
+        ResponseEntity<String> response = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .getForEntity("/cashcards/102", String.class); // kumar2's data
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
